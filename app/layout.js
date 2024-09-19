fad74585d3bd8ac790da884f84b3f634a0f963f5
Abu Shaleh <abushalehtoha@gmail.com>
2024-09-19 19:27:34 +0600

    added header and footer section in layout.js file

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
-
+// "use client"
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import "./globals.css";
-
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="flex flex-col bg-slate-300">
+      <body className="flex flex-col min-h-screen">
+        <Header />
 
+        {/* Use flex-grow for the main section to take up remaining space */}
+        <main className="flex-grow p-1 overflow-y-auto">
+          {children}
+        </main>
 
-        {children}
-
-        
+        <Footer />
       </body>
     </html>
   );
